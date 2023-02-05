e03b297ea76aaa53334a52dd0a4fe992d7c413a8
KamaleshAshok <kamalesh.ashok@gmail.com>
2023-02-05 12:11:54 +0530

    first commit

---
+++ AdminTemplate/Data/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminTemplate.Data
+{
+    public class LoginModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
